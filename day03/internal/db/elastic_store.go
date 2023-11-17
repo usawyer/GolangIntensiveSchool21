e3add3f9e1d53bda50e05c93826c7292bcf319fd
yarina <yarinabelousova@gmail.com>
2023-11-17 14:03:34 +0700

    ex01 getplaces

--- day03/internal/db/elastic_store.go
+++ day03/internal/db/elastic_store.go
@@ -1,7 +1,9 @@
 package db
 
 import (
+	"encoding/json"
 	"github.com/elastic/go-elasticsearch/v8"
+	"io"
 	"search/internal/types"
 )
 
@@ -9,10 +11,45 @@ type ElasticStore struct {
 	client *elasticsearch.Client
 }
 
-func NewElasticStore(client *elasticsearch.Client) *ElasticStore {
-	return &ElasticStore{client: client}
+func NewElasticStore() (*ElasticStore, error) {
+	cfg := elasticsearch.Config{Addresses: []string{"http://localhost:9200"}}
+	es, err := elasticsearch.NewClient(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &ElasticStore{client: es}, nil
 }
 
 func (es *ElasticStore) GetPlaces(limit int, offset int) ([]types.Place, int, error) {
-	return nil, 0, nil
+	res, err := es.client.Search(
+		es.client.Search.WithIndex("places"),
+		es.client.Search.WithSize(limit),
+		es.client.Search.WithFrom(offset),
+		es.client.Search.WithTrackTotalHits(true),
+	)
+
+	if err != nil {
+		return nil, 0, err
+	}
+
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	defer res.Body.Close()
+
+	var responses types.Response
+	if err := json.Unmarshal(bodyBytes, &responses); err != nil {
+		return nil, 0, err
+	}
+
+	totalHits := responses.Hits.Total.Value
+	var places []types.Place
+
+	for _, response := range responses.Hits.Hits {
+		places = append(places, response.Source)
+	}
+
+	return places, totalHits, nil
 }
--- day03/internal/types/types.go
+++ day03/internal/types/types.go
@@ -12,3 +12,20 @@ type Location struct {
 	Lon float64 `json:"lon"`
 	Lat float64 `json:"lat"`
 }
+
+type Response struct {
+	Hits Hits `json:"hits"`
+}
+
+type Hits struct {
+	Total Total `json:"total"`
+	Hits  []Hit `json:"hits"`
+}
+
+type Total struct {
+	Value int `json:"value"`
+}
+
+type Hit struct {
+	Source Place `json:"_source"`
+}
