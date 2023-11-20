ab9a418ef8a45a64d33b9bb0174cef86ee64909a
yarina <yarinabelousova@gmail.com>
2023-11-20 19:22:42 +0700

    ex02

---
+++ day03/cmd/ex02/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"search/internal/db"
+	"search/internal/domain"
+)
+
+func main() {
+	store, err := db.NewElasticStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := domain.NewServer(store)
+
+	http.HandleFunc("/api/places", server.GetPlacesJSONHandler)
+
+	err = http.ListenAndServe(":8888", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- day03/internal/domain/handler.go
+++ day03/internal/domain/handler.go
@@ -1,16 +1,21 @@
 package domain
 
 import (
-	"github.com/pkg/errors"
+	"encoding/json"
 	"html/template"
 	"net/http"
 	"search/internal/db"
 	"search/internal/types"
 	"strconv"
+	"strings"
 )
 
 type Server struct {
 	store *db.ElasticStore
+}
+
+func NewServer(store *db.ElasticStore) *Server {
+	return &Server{store: store}
 }
 
 type Data struct {
@@ -23,14 +28,10 @@ type Data struct {
 	LastPage     int
 }
 
-func NewServer(store *db.ElasticStore) *Server {
-	return &Server{store: store}
-}
-
 func (server *Server) GetPlacesHandler(w http.ResponseWriter, r *http.Request) {
 	data, err := server.PrepareData(r)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if err.Message != "" {
+		http.Error(w, err.Message, http.StatusInternalServerError)
 		return
 	}
 
@@ -40,11 +41,59 @@ func (server *Server) GetPlacesHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (server *Server) PrepareData(r *http.Request) (Data, error) {
+type JsonResponse struct {
+	Name     string        `json:"name"`
+	Total    int           `json:"total"`
+	Places   []types.Place `json:"places"`
+	PrevPage int           `json:"prev_page"`
+	NextPage int           `json:"next_page"`
+	LastPage int           `json:"last_page"`
+}
+
+type CustomError struct {
+	Message string `json:"error"`
+}
+
+func (server *Server) GetPlacesJSONHandler(w http.ResponseWriter, r *http.Request) {
+	data, custom := server.PrepareData(r)
+	if custom.Message != "" {
+		if strings.Contains(custom.Message, "Invalid 'page' value") {
+			WriteJSON(w, CustomError{custom.Message})
+		} else {
+			http.Error(w, custom.Message, http.StatusInternalServerError)
+		}
+		return
+	}
+
+	response := JsonResponse{
+		Name:     "Places",
+		Total:    data.Total,
+		Places:   data.Places,
+		PrevPage: data.PreviousPage,
+		NextPage: data.NextPage,
+		LastPage: data.LastPage,
+	}
+
+	WriteJSON(w, response)
+}
+
+func WriteJSON(w http.ResponseWriter, response interface{}) {
+	jsonResp, err := json.MarshalIndent(response, "", "  ")
+	if err != nil {
+		http.Error(w, "Error encoding JSON response: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(jsonResp)
+}
+
+func (server *Server) PrepareData(r *http.Request) (Data, CustomError) {
 	pageParam := r.URL.Query().Get("page")
 	page, err := strconv.Atoi(pageParam)
 	if err != nil || page < 1 {
-		return Data{}, errors.New("Invalid 'page' value " + pageParam)
+		return Data{}, CustomError{"Invalid 'page' value " + pageParam}
 	}
 
 	limit := 10
@@ -52,7 +101,7 @@ func (server *Server) PrepareData(r *http.Request) (Data, error) {
 
 	places, total, err := server.store.GetPlaces(limit, offset)
 	if err != nil {
-		return Data{}, errors.New("Error fetching places: " + err.Error())
+		return Data{}, CustomError{"Error fetching places: " + err.Error()}
 	}
 
 	answer := Data{
@@ -73,8 +122,8 @@ func (server *Server) PrepareData(r *http.Request) (Data, error) {
 	answer.LastPage = (total + limit - 1) / limit
 
 	if page > answer.LastPage {
-		return Data{}, errors.New("Invalid 'page' value " + pageParam)
+		return Data{}, CustomError{"Invalid 'page' value " + pageParam}
 	}
 
-	return answer, nil
+	return answer, CustomError{}
 }
