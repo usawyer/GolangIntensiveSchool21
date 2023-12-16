81b504444b7f5852ddeef2a7d0b4ce7426f28609
yarina <yarinabelousova@gmail.com>
2023-12-16 14:00:52 +0700

    day06 still

--- day06/cmd/ex01/main.go
+++ day06/cmd/ex01/main.go
@@ -9,14 +9,53 @@ import (
 )
 
 func mainPageHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("web/template/test.gohtml"))
+	redirect(w, r)
+
+	tmpl := template.Must(template.ParseFiles("web/template/main.gohtml", "web/template/header.gohtml"))
 	if err := tmpl.Execute(w, nil); err != nil {
 		http.Error(w, "Error executing template: "+err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func adminPageHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("ADMIN PAGE"))
+	redirect(w, r)
+
+	tmpl := template.Must(template.ParseFiles("web/template/admin.gohtml", "web/template/header.gohtml"))
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, "Error executing template: "+err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		action := r.FormValue("action")
+		switch action {
+		case "login":
+			http.Redirect(w, r, "/admin", http.StatusSeeOther)
+			return
+		case "back":
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		case "submit":
+			login := r.FormValue("login")
+			password := r.FormValue("password")
+
+			if login != "" && password != "" {
+				if login == "user" && password == "user" {
+					log.Println("SUCCESS")
+				} else {
+					log.Println("WRONG")
+				}
+			}
+		}
+
+	}
+
 }
 
 func newRouter() *mux.Router {
