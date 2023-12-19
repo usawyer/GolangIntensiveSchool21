13377b8ae9f2e43f5138f4c553258b9ee09928c6
yarina <yarinabelousova@gmail.com>
2023-12-19 15:23:54 +0700

    day08 ex00

---
+++ day08/internal/arithmetic/arithmetic.go
@@ -0,0 +1,22 @@
+package arithmetic
+
+import (
+	"github.com/pkg/errors"
+	"strconv"
+	"unsafe"
+)
+
+func GetElement(arr []int, idx int) (int, error) {
+	if idx < 0 {
+		return 0, errors.New("index is negative")
+	} else if idx >= len(arr) {
+		return 0, errors.New("index " + strconv.Itoa(idx) + " is out of range")
+	} else if arr == nil || len(arr) == 0 {
+		return 0, errors.New("incorrect array")
+	}
+
+	ptrStart := unsafe.Pointer(&arr[0])
+	itemSize := unsafe.Sizeof(arr[0])
+	element := *(*int)(unsafe.Add(ptrStart, uintptr(idx)*itemSize))
+	return element, nil
+}
