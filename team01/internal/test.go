646335e7bd1c2ea5284c538098d00f7e01576a30
yarina <yarinabelousova@gmail.com>
2024-01-21 19:16:29 +0700

    team01

---
+++ team01/internal/test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		scanner.Scan()
+		txt := scanner.Text()
+		str := strings.Split(txt, " ")
+
+		switch str[0] {
+		case "GET":
+			fmt.Println("GET operation")
+		case "SET":
+			fmt.Println("SET operation")
+		case "DELETE":
+			fmt.Println("DELETE operation")
+		default:
+			fmt.Println("Unknown operation")
+		}
+	}
+
+}
